ecc429e0052b83bf97a929f85da7eeab6ff55571
Eduardo Lima Mitev <elima@igalia.com>
2012-11-28 17:48:09 +0100

    Adds new class FileteaSource which will replace FileSource

---
+++ filetea/filetea-source.c
@@ -0,0 +1,167 @@
+/*
+ * filetea-source.c
+ *
+ * FileTea, low-friction file sharing <http://filetea.net>
+ *
+ * Copyright (C) 2011-2014, Igalia S.L.
+ *
+ * Authors:
+ *   Eduardo Lima Mitev <elima@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * version 3, or (at your option) any later version as published by
+ * the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License at http://www.gnu.org/licenses/agpl.html
+ * for more details.
+ */
+
+#include "filetea-source.h"
+
+G_DEFINE_TYPE (FileteaSource, filetea_source, G_TYPE_OBJECT)
+
+#define FILETEA_SOURCE_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
+                                         FILETEA_SOURCE_TYPE, \
+                                         FileteaSourcePrivate))
+
+/* private data */
+struct _FileteaSourcePrivate
+{
+  gchar *id;
+  EvdPeer *peer;
+  guint flags;
+  gchar *name;
+  gchar *type;
+  gsize size;
+  gchar **tags;
+};
+
+static void     filetea_source_class_init         (FileteaSourceClass *class);
+static void     filetea_source_init               (FileteaSource *self);
+
+static void     filetea_source_finalize           (GObject *obj);
+static void     filetea_source_dispose            (GObject *obj);
+
+static void
+filetea_source_class_init (FileteaSourceClass *class)
+{
+  GObjectClass *obj_class = G_OBJECT_CLASS (class);
+
+  obj_class->dispose = filetea_source_dispose;
+  obj_class->finalize = filetea_source_finalize;
+
+  g_type_class_add_private (obj_class, sizeof (FileteaSourcePrivate));
+}
+
+static void
+filetea_source_init (FileteaSource *self)
+{
+  FileteaSourcePrivate *priv;
+
+  priv = FILETEA_SOURCE_GET_PRIVATE (self);
+  self->priv = priv;
+
+  priv->tags = NULL;
+}
+
+static void
+filetea_source_dispose (GObject *obj)
+{
+  FileteaSource *self = FILETEA_SOURCE (obj);
+
+  if (self->priv->peer != NULL)
+    {
+      g_object_unref (self->priv->peer);
+      self->priv->peer = NULL;
+    }
+
+  G_OBJECT_CLASS (filetea_source_parent_class)->dispose (obj);
+}
+
+static void
+filetea_source_finalize (GObject *obj)
+{
+  FileteaSource *self = FILETEA_SOURCE (obj);
+
+  g_free (self->priv->id);
+  g_free (self->priv->name);
+  g_free (self->priv->type);
+  g_strfreev (self->priv->tags);
+
+  G_OBJECT_CLASS (filetea_source_parent_class)->finalize (obj);
+}
+
+/* public methods */
+
+FileteaSource *
+filetea_source_new (EvdPeer      *peer,
+                    const gchar  *name,
+                    const gchar  *type,
+                    gsize         size,
+                    guint         flags,
+                    const gchar **tags)
+{
+  FileteaSource *self;
+
+  g_return_val_if_fail (EVD_IS_PEER (peer), NULL);
+  g_return_val_if_fail (name != NULL, NULL);
+  g_return_val_if_fail (type != NULL, NULL);
+
+  self = g_object_new (FILETEA_SOURCE_TYPE, NULL);
+
+  self->priv->peer = peer;
+  g_object_ref (peer);
+
+  self->priv->name = g_strdup (name);
+  self->priv->type = g_strdup (type);
+  self->priv->size = size;
+  self->priv->flags = flags;
+  if (tags != NULL)
+    self->priv->tags = g_strdupv ((gchar **) tags);
+
+  return self;
+}
+
+const gchar *
+filetea_source_get_name (FileteaSource *self)
+{
+  g_return_val_if_fail (FILETEA_IS_SOURCE (self), NULL);
+
+  return self->priv->name;
+}
+
+const gchar *
+filetea_source_get_content_type (FileteaSource *self)
+{
+  g_return_val_if_fail (FILETEA_IS_SOURCE (self), NULL);
+
+  return self->priv->type;
+}
+
+gsize
+filetea_source_get_size (FileteaSource *self)
+{
+  g_return_val_if_fail (FILETEA_IS_SOURCE (self), 0);
+
+  return self->priv->size;
+}
+
+guint
+filetea_source_get_flags (FileteaSource *self)
+{
+  g_return_val_if_fail (FILETEA_IS_SOURCE (self), 0);
+
+  return self->priv->flags;
+}
+
+const gchar **
+filetea_source_get_tags (FileteaSource *self)
+{
+  g_return_val_if_fail (FILETEA_IS_SOURCE (self), 0);
+
+  return (const gchar **) self->priv->tags;
+}
---
+++ filetea/filetea-source.h
@@ -0,0 +1,82 @@
+/*
+ * filetea-source.h
+ *
+ * FileTea, low-friction file sharing <http://filetea.net>
+ *
+ * Copyright (C) 2011-2014, Igalia S.L.
+ *
+ * Authors:
+ *   Eduardo Lima Mitev <elima@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * version 3, or (at your option) any later version as published by
+ * the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License at http://www.gnu.org/licenses/agpl.html
+ * for more details.
+ */
+
+#ifndef __FILETEA_SOURCE_H__
+#define __FILETEA_SOURCE_H__
+
+#include <evd.h>
+
+G_BEGIN_DECLS
+
+typedef struct _FileteaSource FileteaSource;
+typedef struct _FileteaSourceClass FileteaSourceClass;
+typedef struct _FileteaSourcePrivate FileteaSourcePrivate;
+
+typedef enum {
+  FILETEA_SOURCE_FLAGS_NONE          = 0,
+  FILETEA_SOURCE_FLAGS_PUBLIC        = 1 << 0,
+  FILETEA_SOURCE_FLAGS_LIVE          = 1 << 1,
+  FILETEA_SOURCE_FLAGS_REAL_TIME     = 1 << 2,
+  FILETEA_SOURCE_FLAGS_CHUNKABLE     = 1 << 3,
+  FILETEA_SOURCE_FLAGS_BIDIRECTIONAL = 1 << 4
+} FileteaSourceFlags;
+
+struct _FileteaSource
+{
+  EvdWebService parent;
+
+  FileteaSourcePrivate *priv;
+};
+
+struct _FileteaSourceClass
+{
+  EvdWebServiceClass parent_class;
+
+  /* signal prototypes */
+};
+
+#define FILETEA_SOURCE_TYPE           (filetea_source_get_type ())
+#define FILETEA_SOURCE(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), FILETEA_SOURCE_TYPE, FileteaSource))
+#define FILETEA_SOURCE_CLASS(obj)     (G_TYPE_CHECK_CLASS_CAST ((obj), FILETEA_SOURCE_TYPE, FileteaSourceClass))
+#define FILETEA_IS_SOURCE(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), FILETEA_SOURCE_TYPE))
+#define FILETEA_IS_SOURCE_CLASS(obj)  (G_TYPE_CHECK_CLASS_TYPE ((obj), FILETEA_SOURCE_TYPE))
+#define FILETEA_SOURCE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), FILETEA_SOURCE_TYPE, FileteaSourceClass))
+
+
+GType             filetea_source_get_type                (void) G_GNUC_CONST;
+
+FileteaSource *   filetea_source_new                     (EvdPeer      *peer,
+                                                          const gchar  *name,
+                                                          const gchar  *type,
+                                                          gsize         size,
+                                                          guint         flags,
+                                                          const gchar **tags);
+
+const gchar *     filetea_source_get_name                (FileteaSource  *self);
+const gchar *     filetea_source_get_content_type        (FileteaSource  *self);
+gsize             filetea_source_get_size                (FileteaSource  *self);
+guint             filetea_source_get_flags               (FileteaSource  *self);
+const gchar **    filetea_source_get_tags                (FileteaSource  *self);
+
+G_END_DECLS
+
+#endif /* __FILETEA_SOURCE_H__ */
